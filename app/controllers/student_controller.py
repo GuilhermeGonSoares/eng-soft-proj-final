c526d7e416380f58f1f96a5fbe4d4eb9e2686160
Guilherme Goncalves Soares <guilherme.soares@tc.df.gov.br>
2023-07-24 17:07:15 -0300

    estudante resolver a prova

--- app/controllers/student_controller.py
+++ app/controllers/student_controller.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_file
 from flask_login import current_user, login_required
-from datetime import datetime, timedelta
 from ..models import *
 from ..forms import *
 from app import db
-from sqlalchemy import or_
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 bp = Blueprint('student', __name__)
 
@@ -28,184 +28,95 @@ def index():
 @bp.route('/responder_teste/<int:teste_id>', methods=['GET', 'POST'])
 @login_required
 def answer_test(teste_id):
-    teste = Teste.query.get(teste_id)
-    if not teste:
-        return "Teste não encontrado", 404
-
+    teste = Teste.query.get_or_404(teste_id)
     if teste.status != StatusTeste.ABERTO:
-        return "Teste não está aberto", 403
+        flash("O teste não está aberto para respostas.", category='danger')
+        return redirect(url_for('student.index'))
 
-    caderno_respostas = CadernoRespostas.query.filter_by(teste_id=teste_id, aluno_matricula=current_user.matricula).first()
-    if not caderno_respostas:
-        caderno_respostas = CadernoRespostas(teste_id=teste_id, aluno_matricula=current_user.matricula)
-        db.session.add(caderno_respostas)
-        db.session.commit()
+    if request.method == 'GET':
 
-    form = TesteForm()
-    for i, questao in enumerate(teste.questoes):
-        if questao.tipo == TipoQuestao.MULTIPLA_ESCOLHA:
-            form.multipla_escolha.append_entry()
-            form.multipla_escolha[i].opcao.choices = [(opcao.id, opcao.texto) for opcao in questao.opcoes]
-        elif questao.tipo == TipoQuestao.VERDADEIRO_FALSO:
-            form.verdadeiro_falso.append_entry()
-        elif questao.tipo == TipoQuestao.DISCURSIVA:
-            form.discursiva.append_entry()
-
-    if form.validate_on_submit():
-        for i, questao in enumerate(teste.questoes):
-            if questao.tipo == TipoQuestao.MULTIPLA_ESCOLHA:
-                resposta = Resposta(caderno_respostas_id=caderno_respostas.id, questao_id=questao.id, resposta=form.multipla_escolha[i].opcao.data, acertou=form.multipla_escolha[i].opcao.data == questao.gabarito)
-            elif questao.tipo == TipoQuestao.VERDADEIRO_FALSO:
-                resposta = Resposta(caderno_respostas_id=caderno_respostas.id, questao_id=questao.id, resposta=form.verdadeiro_falso[i].opcao.data, acertou=form.verdadeiro_falso[i].opcao.data == questao.gabarito)
-            elif questao.tipo == TipoQuestao.DISCURSIVA:
-                resposta = Resposta(caderno_respostas_id=caderno_respostas.id, questao_id=questao.id, resposta=form.discursiva[i].resposta.data)
-            db.session.add(resposta)
-        db.session.commit()
-        return redirect(url_for('testes'))
-        
-    return render_template('pages/answer_test.html', form=form, questoes=teste.questoes)
-
-
-
-# choices_ans = {
-#     '1': 'A',
-#     '2': 'B',
-#     '3': 'C',
-#     '4': 'D'
-# }
-
-# def formatar_tempo_restante(tempo_restante):
-#     segundos_totais = tempo_restante.total_seconds()
-#     horas = int(segundos_totais // 3600)
-#     minutos = int((segundos_totais % 3600) // 60)
-#     segundos = int(segundos_totais % 60)
-#     return f"{horas:02d}:{minutos:02d}:{segundos:02d}"
-
-
-# def descrever_teste(testes_disponiveis):
-#     testes_info = []
-#     now = datetime.utcnow()
+        return render_template('pages/teste.html', teste=teste, questoes=teste.questoes)
     
-#     for teste in testes_disponiveis:
-#         questoes_count = len(teste.questoes)
-        
-#         tempo_restante = None
-#         if teste.created_at:
-#             duracao = timedelta(minutes=teste.duracao)
-#             tempo_final = teste.created_at + duracao
-#             tempo_restante = tempo_final - now
-        
-#         teste_info = {
-#             'teste': teste,
-#             'questoes_count': questoes_count,
-#             'tempo_restante': formatar_tempo_restante(tempo_restante),
-#         }
-        
-#         testes_info.append(teste_info)
+    if request.method == 'POST':
+        cadernoResposta = CadernoRespostas.query.filter_by(teste_id=teste_id, aluno_matricula=current_user.matricula).first()
+        if cadernoResposta:
+            flash('Está prova só aceita uma tentativa de resposta', category='danger')
+            return redirect(url_for('student.index'))
 
-#     return testes_info
+        nota = 0
+        cadernoResposta = CadernoRespostas(
+            aluno_matricula=current_user.matricula,
+            teste_id=teste_id,
+            finalizado=True
+        )
 
+        db.session.add(cadernoResposta)
+        db.session.flush()
 
-# @bp.route('/prova')
-# @login_required
-# def index():
-#     testes = Teste.query.all()
-#     testes_disponiveis = []
-#     testes_concluidos = []
-
-#     for teste in testes:
-#         caderno_resposta = CadernoRespostas.query.filter_by(
-#             aluno_matricula=current_user.matricula,
-#             teste_id=teste.id
-#             ).first()
-#         nota = 0
-#         if caderno_resposta:
-#             nota = caderno_resposta.nota
-#         if teste.ativo == False:
-#             testes_concluidos.append((teste, nota))
-#         else:
-#             if caderno_resposta and caderno_resposta.finalizado:
-#                 testes_concluidos.append((teste, nota))
-#             else:
-#                 testes_disponiveis.append(teste)
-    
-#     testes_info = descrever_teste(testes_disponiveis)
-    
-#     return render_template('pages/student.html', 
-#                            testes_info=testes_info,
-#                            testes_info_concluidos=testes_concluidos
-#                            )
-
-
-# @bp.route('/prova/<int:teste_id>/questao/<int:questao_id>', methods=['GET', 'POST'])
-# @login_required
-# def show(teste_id, questao_id):
-#     matricula = current_user.matricula
-    
-#     caderno_respostas = CadernoRespostas.query.filter_by(aluno_matricula=matricula, teste_id=teste_id).first()
-#     print(caderno_respostas)
-#     if not caderno_respostas:
-#         caderno_respostas = CadernoRespostas(aluno_matricula=matricula, teste_id=teste_id)
-#         db.session.add(caderno_respostas)
-#         db.session.commit()
-    
-#     resposta = Resposta.query.filter_by(
-#             caderno_respostas=caderno_respostas,
-#             questao_id=questao_id
-#         ).first()
-
-#     if request.method == 'POST':
-#         alternativa_selecionada = request.form.get('options')
-#         print('alternativa_selecionada', alternativa_selecionada)
-#         questao = Questao.query.get(questao_id)
-        
-#         if not resposta:
-#             nova_resposta = Resposta(
-#                 caderno_respostas=caderno_respostas,
-#                 questao_id=questao_id,
-#                 resposta=alternativa_selecionada,
-#                 acertou = alternativa_selecionada == questao.gabarito
-#             )
-#             db.session.add(nova_resposta)
-#             db.session.commit()
-#         else:
-#             atualizar_resposta = resposta
-#             atualizar_resposta.resposta = alternativa_selecionada
-#             atualizar_resposta.acertou = alternativa_selecionada == questao.gabarito
-#             db.session.add(atualizar_resposta)
-#             db.session.commit()
-        
-#         if request.form.get('action') == 'finalizar':
-#             todas_respostas = Resposta.query.filter_by(caderno_respostas=caderno_respostas).all()
-#             nota = 0
-#             for res in todas_respostas:
-#                 if res.acertou:
-#                     nota += int(res.questao.pontuacao)
+        for questao in teste.questoes:
+            resposta = Resposta(
+                caderno_respostas_id=cadernoResposta.id,
+                questao_id=questao.id
+            )
+            # Obtenha a resposta para esta questão do formulário
+            resposta_form = request.form.get(str(questao.id))
             
-#             caderno_respostas.finalizado = True
-#             caderno_respostas.nota = nota
-#             db.session.add(caderno_respostas)
-#             db.session.commit()
-#             return redirect(url_for('student.index'))
+            resposta.acertou = resposta_form == questao.gabarito
+            resposta.resposta = resposta_form
+            if resposta.acertou :
+                nota += questao.pontuacao
 
-#         return redirect(url_for('student.show', teste_id=teste_id, questao_id=questao_id + 1))
+            db.session.add(resposta)
+        
+        cadernoResposta.nota = nota
 
-#     else:
-        
-#         teste = Teste.query.get(teste_id)
-#         test_info = descrever_teste([teste])[0]
-#         questao = next(filter(lambda q: q.id == questao_id, teste.questoes), None)
-#         questao_anterior =  next(filter(lambda q: q.id == questao_id - 1, teste.questoes), None)
-#         proxima_questao =  next(filter(lambda q: q.id == questao_id + 1, teste.questoes), None)
-#         questao_number = teste.questoes.index(questao)
-        
-        
-#         return render_template('pages/exame.html', 
-#                             test_info = test_info, 
-#                             questao=questao, 
-#                             questao_anterior=questao_anterior,
-#                             proxima_questao=proxima_questao,
-#                             questao_number=questao_number + 1,
-#                             resposta=resposta.resposta if resposta else None
-#                             )
+        db.session.commit()
+        flash('Caderno resposta salvo com sucesso!', 'success')
+        return redirect(url_for('student.index'))
 
+@bp.route('/relatorio/<int:teste_id>', methods=['GET', 'POST'])
+@login_required
+def relatorio_teste(teste_id):
+    # Fetch the answer notebook for this test and student
+    cadernoResposta = CadernoRespostas.query.filter_by(teste_id=teste_id, aluno_matricula=current_user.matricula).first()
+
+    # If no answer notebook exists, return an error
+    if not cadernoResposta:
+        flash('Não há respostas para este teste.', category='danger')
+        return redirect(url_for('student.index'))
+        
+    # Fetch the test
+    teste = Teste.query.get_or_404(teste_id)
+    
+    # Fetch the questions for the test
+    questoes = teste.questoes
+
+    # Start generating the PDF
+    c = canvas.Canvas("relatorios/relatorio.pdf", pagesize=letter)
+    width, height = letter
+
+    # Add some custom text
+    c.drawString(100, height - 100, f"Relatório de teste para: {current_user.matricula}")
+    c.drawString(100, height - 120, f"Nome do teste: {teste.nome}")
+    c.drawString(100, height - 140, f"Nota final: {cadernoResposta.nota}")
+    
+    line_height = 150
+    for questao, resposta in zip(questoes, cadernoResposta.respostas):
+        line_height += 15
+        c.drawString(100, height - line_height, f"Nome da questão: {questao.nome}")
+        line_height += 15
+        c.drawString(100, height - line_height, f"Enunciado da questão: {questao.texto}")
+        line_height += 15
+        c.drawString(100, height - line_height, f"Pontuação da questão: {questao.pontuacao}")
+        line_height += 15
+        c.drawString(100, height - line_height, f"Gabarito da questão: {questao.gabarito}")
+        line_height += 15
+        c.drawString(100, height - line_height, f"Resposta do aluno: {resposta.resposta}")
+        line_height += 15
+        c.drawString(100, height - line_height, f"Acertou a questão: {'Sim' if resposta.acertou else 'Não'}")
+        line_height += 15
+
+    # Finish up the page and save it
+    c.save()
+
+    return send_file('relatorios/relatorio.pdf', as_attachment=True)
--- app/forms/answer_forms.py
+++ app/forms/answer_forms.py
@@ -1,20 +1,52 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, BooleanField, TextAreaField, FieldList, FormField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, RadioField, IntegerField
+from wtforms.validators import DataRequired, Optional
+from ..models import TipoQuestao  # Importe a enumeração TipoQuestao da sua aplicação
 
-class MultiplaEscolhaForm(FlaskForm):
-    opcao = RadioField('Opções', coerce=int)
+def numero_to_letra(numero):
+    dic = {
+        '1': 'A',
+        '2': 'B',
+        '3': 'C',
+        '4': 'D'
+    }
 
-class VerdadeiroFalsoForm(FlaskForm):
-    opcao = BooleanField('Verdadeiro ou Falso?')
+    return dic[numero]
 
-class DiscursivaForm(FlaskForm):
-    resposta = TextAreaField('Resposta')
+class FormularioResposta(FlaskForm):
+    submit = SubmitField('Enviar Respostas')
 
+    def __init__(self, questoes, *args, **kwargs):
+        self.questoes = questoes
+        super(FormularioResposta, self).__init__(*args, **kwargs)
 
+    def process(self, formdata=None, obj=None, data=None, **kwargs):
+        for questao in self.questoes:
+            if questao.tipo == TipoQuestao.MULTIPLA_ESCOLHA.value:
+                unbound_field = RadioField(
+                    label=questao.nome,
+                    choices=[(numero_to_letra(str(opcao.id)), opcao.texto) for opcao in questao.opcoes],
+                    validators=[DataRequired()]
+                )
+            elif questao.tipo == TipoQuestao.VERDADEIRO_FALSO.value:
+                unbound_field = RadioField(
+                    label=questao.nome,
+                    choices=[('verdadeiro', 'Verdadeiro'), ('falso', 'Falso')],
+                    validators=[DataRequired()]
+                )
+            elif questao.tipo == TipoQuestao.DISCURSIVA.value:
+                unbound_field = IntegerField(
+                    label=questao.nome,
+                    validators=[Optional()]  # Campo discursivo pode ser deixado em branco
+                )
 
-class TesteForm(FlaskForm):
-    multipla_escolha = FieldList(FormField(MultiplaEscolhaForm), min_entries=0)
-    verdadeiro_falso = FieldList(FormField(VerdadeiroFalsoForm), min_entries=0)
-    discursiva = FieldList(FormField(DiscursivaForm), min_entries=0)
-    submit = SubmitField('Submit')
+            # Bind the UnboundField to this form instance
+            bound_field = unbound_field.bind(form=self, name=f'resposta_questao_{questao.id}', prefix=self._prefix)
+
+            # Process the bound field
+            bound_field.process(formdata)
+
+            # Set the attribute on the form instance
+            setattr(self, f'resposta_questao_{questao.id}', bound_field)
+
+        super(FormularioResposta, self).process(formdata, obj, data, **kwargs)
