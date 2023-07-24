5decacca538039d4465b0c0b3e5d0786a8c67c30
gabriel <gabrielcontentcreation@gmail.com>
2023-07-24 19:33:21 -0300

    removendo front antigo

--- novo-front/js/base.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.nav-link');
-
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    links.forEach((l) => l.classList.remove('active'));
-    e.currentTarget.classList.add('active');
-  });
-});
